ff5f5b3f057e0cbbdd902cb3784c2741a04b3a0d
Alexandr Komarov <mr.komarovsasha@mail.ru>
2021-04-20 09:14:43 +0300

    User data validation

--- regex.py
+++ regex.py
@@ -10,18 +10,21 @@ reg_exp ={"mail": r"[\w._-]+@[\w._-]+\.[\w.]+",
           "uri": r"https?://[0-9a-zA-Z-_]+\.[0-9a-zA-Z-_]+[^\s]*"}
 
 #get information from user
-filename_input = input("Enter input file: ")
-filename_output = input ("Enter output file: ")
-search_subject = input ("What do you want to find? Enter one of the options:\nmail, telephone, domain, ipv4, mac, ipv6, uri: ")
+try:
+    filename_input = input("Enter input file: ")
+    filename_output = input ("Enter output file: ")
+    search_subject = input ("What do you want to find? Enter one of the options:\nmail, telephone, domain, ipv4, mac, ipv6, uri: ")
 
-file_input = open(filename_input, mode = 'r')
-file_output = open(filename_output, mode = 'w')
+    file_input = open(filename_input, mode = 'r')
+    file_output = open(filename_output, mode = 'w')
 
-#Finding information and writing to a file
-result = re.findall(reg_exp[search_subject],file_input.read())
-for i in result:
-    file_output.write(i+"\n")
-    print(i+"\n")
+    #Finding information and writing to a file
+    result = re.findall(reg_exp[search_subject],file_input.read())
+    for i in result:
+        file_output.write(i+"\n")
+        print(i+"\n")
+except:
+    print ("!!! Please, check the input and try again !!!")
 
 file_input.close()
 file_output.close()
