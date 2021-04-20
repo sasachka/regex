e3c946bb4891383d9f5042f93b6b7a70af5d38cf
Alexandr Komarov <mr.komarovsasha@mail.ru>
2021-04-20 09:19:43 +0300

    User data validation

--- regex.py
+++ regex.py
@@ -9,8 +9,8 @@ reg_exp ={"mail": r"[\w._-]+@[\w._-]+\.[\w.]+",
           "ipv6": r"(?:[0-9a-fA-F]{0,4}:){2,7}(?:[0-9a-fA-F]{0,4})?",
           "uri": r"https?://[0-9a-zA-Z-_]+\.[0-9a-zA-Z-_]+[^\s]*"}
 
-#get information from user
 try:
+# get information from user:
     filename_input = input("Enter input file: ")
     filename_output = input ("Enter output file: ")
     search_subject = input ("What do you want to find? Enter one of the options:\nmail, telephone, domain, ipv4, mac, ipv6, uri: ")
@@ -18,7 +18,7 @@ try:
     file_input = open(filename_input, mode = 'r')
     file_output = open(filename_output, mode = 'w')
 
-    #Finding information and writing to a file
+#Finding information and writing to a file
     result = re.findall(reg_exp[search_subject],file_input.read())
     for i in result:
         file_output.write(i+"\n")
